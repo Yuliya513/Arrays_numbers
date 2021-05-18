cc291d706f4fb716f19048199780803019e04bc1
Yuliya513 <yuliya.senik@gmail.com>
2021-05-18 16:46:52 +0300

    Initial commit

---
+++ src/com/company/Arrays.Numbers.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Array numbers {
+
+    public static void main(String[] args) {
+        int [5] Array;
+        an Array[0]=-1;
+        an Array[1]=-10;
+        an Array[2]=-100;
+        an Array[3]=30;
+        an Array[4]=40;
+        an Array[5]=-5;
+        System.out.println("Element at index -10;"+an Array [1]);
+        System.out.println("Element at index -1;"+an Array [0]);
+        System.out.println("Element at index 30;"+an Array [3]);
+    }
+}
+
