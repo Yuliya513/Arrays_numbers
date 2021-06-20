e55ffd3657f3903949941193a83fcea4bf5cb93e
Yuliya513 <83278354+Yuliya513@users.noreply.github.com>
2021-06-20 11:18:33 +0300

    online project with some methods

---
+++ src/com/company/Numbers.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class  Numbers {
+
+    public static void main(String[] args) {
+        int[] array =new int[6];
+         array[0]=-1;
+         array[1]=-10;
+         array[2]=-100;
+         array[3]=30;
+         array[4]=40;
+         array[5]=-5;
+        System.out.println("Element at index -10;"+ array [1]);
+        System.out.println("Element at index -1;"+ array [0]);
+        System.out.println("Element at index 30;"+ array [3]);
+    }
+}
+
